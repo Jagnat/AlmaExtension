1bd9eddd235fa3b34ff1d001299053e8aba9c127
Student Technology Training <stctrain@wwu.edu>
2019-09-26 10:00:34 -0700

    remade alma extension for new alma

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 // Listener for a message to open an incognito window with the checkout agreement
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-	{
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.act == "openPage") {
 		chrome.windows.create({"url": "https://esign.wwu.edu/forms/Library/_equipment_checkout_agreement_1.aspx", "incognito": true});
-		sendResponse({test: "allGood"});
-	});
+		sendResponse({result: "opened window"});
+	} else {
+		sendResponse({result: "not openPage request"});
+	};
+});
--- script.js
+++ script.js
@@ -1,179 +1,56 @@
-var TIMEOUT_VALUE = 50;
-var LOAD_DELAY = 100;
-var TIMEOUT_ATTEMPTS_LIMIT = 10;
-var PATRON_SERVICES_TITLE = "Patron Services"
-var MANAGE_ITEM_RETURNS_TITLE = "Manage Item Returns"
 
-var attempt = 1;
+// Constants
+var NOTE_CLASS_NAME = "innerContainer";
+var SEARCH_TERM = "equipment checkout";
+var CONFIRM_MSG = "User hasn't signed checkout agreement!\nOpen page?"
 
 
-var websiteTitle = document.title
+// Load listener
+function load_listener(event) {
+  var events = event['path'];
 
-if (websiteTitle == PATRON_SERVICES_TITLE)
-{
-  window.setTimeout(checkForCheckouts, LOAD_DELAY)
-  
-  // Programmatically click button, call function to wait for loading
-  var btn = document.getElementById("fulfillmentpatronServicesuser_notes");
-  if (btn)
-  {
-    btn.click();
-    window.setTimeout(isLoadingDone, TIMEOUT_VALUE);
-  }
-}
-else if (websiteTitle == MANAGE_ITEM_RETURNS_TITLE)
-{
-  window.setTimeout(checkForCheckins, LOAD_DELAY)
-}
-
-function checkForCheckouts()
-{
-  var BARCODE_PREFIX = "HREF_INPUT_SELENIUM_ID_loanList_ROW_"
-  var BARCODE_SUFFIX = "_COL_itemLoanbarcode"
-  var ITEM_NAME_PREFIX = "SPAN_SELENIUM_ID_returnList_ROW_"
-  var ITEM_NAME_SUFFIX = "_COL_itemLoanbarcode"
-  var PATRON_DETAILS_ID = "EXLPatronDetailsRow"
-  
-  
-  var arrayOfItems = []
-  
-  // This while loop appends i (an incrementing variable) to a specific prefix and suffix, finding the element ids of an item's barcode and name.
-  // Everytime that an element (that was not null) is found, i is incremented.
-  var i = 0
-  while (true)
-  {
-    var id = '' + i
-    
-    var barcode_id = BARCODE_PREFIX + id + BARCODE_SUFFIX 
-    var item_name_id = ITEM_NAME_PREFIX + id + ITEM_NAME_SUFFIX
-    
-    var barcode = document.getElementById(barcode_id).value
-    var item_name = document.getElementById(item_name_id).value
-    
-    // If barcode equals null, this means that there is no more items that are on the 'list'
-    if (barcode == null)
-    {
-      break
+  // Iterate over each event and see if its the DIV we care about
+  for (var i=0; i < events.length; i++) {
+    // If so, handle it and break
+    if (events[i].className == NOTE_CLASS_NAME) {
+      handle_div(events[i]);
+      break;
     }
-    
-    // Adds the array of barcode | item name to the array of items
-    arrayOfItems.push([barcode, item_name])
-    i++
-  }
-  
-  // If i equals 0, this essentially means that arrayOfItems is empty
-  if (i == 0)
-  {
-    return
-  }
-  
-  var patronDetails = document.getElementsById(PATRON_DETAILS_ID)
-  // The first element contains the patron's name in a 'lastName, firstName' format
-  // We grab the first element, split it on the ', ' and grab the second element for the first name
-  var patronFirstName = patronDetails[0].value.split(", ")[1]
-  // The second element is simply the w number
-  var patronWNumber = patronDetails[1].value
-  
-  for (var i = 0; i < arrayOfItems.length; i++)
-  {
-    item = arrayOfItems[i]
-    
-    // TODO: Check database to see if  items is in database table yet, if not, add it in the format of...
-  // itemBarcode | itemName | patronWNumber | patronFirstName
-    // mysqlHandler(?).add(item[0], item[1], patronWNumber, patronFirstName)
   }
 }
 
-function checkForCheckins()
-{
-  var BARCODE_PREFIX = "SPAN_SELENIUM_ID_returnList_ROW_"
-  var BARCODE_SUFFIX = "_COL_itemLoanbarcode"
-  
-  // Similar to checkForCheckouts, this increments i to find element IDs.
-  var i = 0
-  while (true)
-  {
-    var id = '' + i
-    var barcode_id = BARCODE_PREFIX + id + BARCODE_SUFFIX
-    
-    var barcode = document.getElementsById(barcode_id).value
-    
-    if (barcode == null)
-    {
-      break
-    }
-    
-    // TODO: Check database to remove barcode if it exists. Barcode is our key
-    // mysqlHandler(?).remove(barcode)
-    i++
-  }
-}
+// Handles checking user info
+function handle_div(event) {
+  // Get all tables loaded under this content
+  var tables = event.getElementsByTagName("table");
+  var popup = true;
 
-// Recursively check and see if loading has finished
-function isLoadingDone()
-{
-	var e = document.getElementById("fulfillmentpatronServicesuser_notes_span")
-	if (e.className.includes("tabSelected"))
-	{
-		// If we are loaded
-		checkUserNotes();
-    return
-	}
-  
-  // Otherwise, time out again
-  attempt++
-  
-  if (attempt > TIMEOUT_ATTEMPTS_LIMIT) {
-    console.log("Timeout on loading user notes tab")
-    return
-  }
-  
-  window.setTimeout(isLoadingDone, TIMEOUT_VALUE);
-}
+  // Iterate over each table and see if its inner text has the keyword we want
+  for (var i=0; i < tables.length; i++) {
+    var table_text = tables[i].innerText.toLowerCase();
 
-function checkUserNotes()
-{
-	var e = document.getElementsByTagName("span");
-
-	var shouldPopup = true;
-
-  
-  // If an item has been scanned, we don't need to popup
-	if (document.getElementById("TABLE_DATA_loanList"))
-	{
-		shouldPopup = false
-	}
-  else
-  {
-    // Iterate through span elements, search for required text
-    for (var i = 0; i < e.length; i++)
-    {
-      var text = e[i].innerHTML.toLowerCase();
-      if (text.includes("equipment checkout"))
-      {
-        // If we found text, we don't need to popup
-        shouldPopup = false;
-        break
-      }
+    // If so, the user is free to checkout and no popup is wanted
+    if (table_text.includes(SEARCH_TERM)) {
+      popup = false;
+      break;
     }
   }
 
-	if (shouldPopup)
-	{
-		console.log("User doesn't have permissions!");
-		// Open confirmation dialog box
-		if (window.confirm("User hasn't signed checkout agreement! Open page?"))
-		{
-			// If "ok" clicked, send a message to the background script
-			// to open up an incognito window
-			chrome.runtime.sendMessage({act: "openPage"}, function(response)
-			{
-				console.log(response.test);
-			});
-		}
-	}
-	else
-	{
-		console.log("User has permissions!");
-	}
+  // Otherwise we send a message to a background script to make a new incognito window
+  if (popup) {
+    console.log("User doesn't have permissions!");
+
+    // Open confirmation dialog box
+    if (window.confirm(CONFIRM_MSG)) {
+      chrome.runtime.sendMessage({act: "openPage"}, function(response) {
+        console.log(response.result);
+      });
+    }
+  } else {
+    console.log("User has permissions!");
+  }
 }
+
+// Register a "load" event listener which is triggered whenever content is told to load
+console.log("loaded");
+document.addEventListener("load", load_listener, true);
